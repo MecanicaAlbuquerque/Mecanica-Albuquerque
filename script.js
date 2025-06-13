ff07d2500d14d28476c84e1e450dd62ffc241cae
MecanicaAlbuquerque <mecanicaalbuquerquedf@gmail.com>
2025-06-13 18:15:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -153,3 +153,9 @@ document.addEventListener("touchmove", function(event) {
         }
     }
 }, { passive: false });
+
+document.addEventListener("touchmove", function(event) {
+    if (event.touches.length === 1) {
+        event.preventDefault(); // Bloqueia movimento lateral
+    }
+}, { passive: false });
